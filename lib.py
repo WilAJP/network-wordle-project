c2e58478dcec44bdcf334aa7fe4933dac630c5c5
Chris Frias <chrisfrias2020@gmail.com>
2025-11-24 10:54:10 -0500

    Update lib.py

--- lib.py
+++ lib.py
@@ -10,11 +10,27 @@ def load_words(filename="words.txt"):
 def get_random_word(words):
     return random.choice(words)
 
-# sends a message across the socket
-def send_msg(conn, msg):
-    conn.sendall((msg + "\n").encode())
 
-# receives a message from the socket
-def recv_msg(conn):
-    data = conn.recv(1024).decode().strip()
-    return data
+
+def returnColor(guess, word):
+    # default color grey
+    result = ["_"] * len(guess)
+
+    # count letters in word
+    counts = {}
+    for ch in word:
+        counts[ch] = counts.get(ch, 0) + 1
+
+    # loop through check either green or yellow
+    for i in range(len(guess)):
+        if guess[i] == word[i]:
+            result[i] = "G"
+            counts[guess[i]] -= 1
+        else:
+            # yellow if in dict 
+            if guess[i] in counts and counts[guess[i]] > 0:
+                result[i] = "Y"
+                counts[guess[i]] -= 1
+
+    return result
+
